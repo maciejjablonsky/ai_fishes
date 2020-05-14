57a7f05948ea01e5fa89ad001c6c00608af71c75
maciejjablonsky <maciekjablonsky@gmail.com>
2020-05-15 01:19:24 +0200

    Added reaction area for agents and debug printing

--- aifishes/fish.py
+++ aifishes/fish.py
@@ -1,6 +1,7 @@
 import numpy as np
 import aifishes.config as cfg
 
+from aifishes import agent
 from aifishes.agent import Agent, random_position, random_velocity
 import pygame as pg
 
@@ -28,3 +29,22 @@ class Fish(Agent):
     def limit_velocity(self):
         limit = cfg.fish_vel_max_magnitude()
         return super().limit_velocity(limit=limit)
+
+    def reaction_area(self):
+        direction_angle = agent.X_AXIS_VEC.angle_to(self.velocity)     
+        radius = cfg.fish()['reaction_radius']
+        vis_angle = cfg.fish()['vision_angle']
+        start = agent.scale(direction_angle - vis_angle/ 2, [0, 360], [0, 2*np.pi])
+        end = agent.scale(direction_angle + vis_angle / 2, [0, 360], [0, 2*np.pi])
+        t = np.linspace(start, end, num=20, dtype=np.float32)
+        x = np.append(0, radius * np.cos(t))
+        y = np.append(0, radius * np.sin(t))
+        area = np.c_[x, y]
+        return area + self.position
+
+    def debug_print(self, screen:pg.Surface):
+        pg.draw.circle(screen, (255, 0, 0), np.array(self.position, dtype=np.int32), 3)
+        sprite_dim = pg.Vector2(self.showable_sprite.get_size())
+        pg.draw.rect(screen, (0, 255, 0), pg.Rect(self.position - sprite_dim/2, sprite_dim), 2)
+        pg.draw.polygon(screen, (0, 0, 0), self.reaction_area(), 2)
+
--- aifishes/predator.py
+++ aifishes/predator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import aifishes.config as cfg
+from aifishes import agent
 from aifishes.agent import Agent, random_position, random_velocity
 import pygame as pg
 
@@ -10,9 +11,9 @@ PREDATOR_SPRITE = None
 def predator_sprite():
     global PREDATOR_SPRITE
     if PREDATOR_SPRITE is None:
-        dim = np.array(cfg.predator_dim())
-        surf = pg.Surface(dim, pg.SRCALPHA)
-        shape = np.array([[0, 0], [dim[0], 0.5 * dim[1]],[0, dim[1]]], dtype=np.float32)
+        w, h = tuple(cfg.predator_dim())
+        surf = pg.Surface((w, h), pg.SRCALPHA)
+        shape = np.array([[0, 0], [w, h/2],[0, h]], dtype=np.float32)
         pg.gfxdraw.aapolygon(surf, shape, PREDATOR_COLOR)
         pg.gfxdraw.filled_polygon(surf, shape, PREDATOR_COLOR)
         PREDATOR_SPRITE = surf
@@ -27,4 +28,22 @@ class Predator(Agent):
         limit = cfg.predator_vel_max_magnitude()
         return super().limit_velocity(limit=limit)
 
-    
+    def reaction_area(self):
+        direction_angle = agent.X_AXIS_VEC.angle_to(self.velocity)     
+        radius = cfg.predator()['reaction_radius']
+        vis_angle = cfg.predator()['vision_angle']
+        start = agent.scale(direction_angle - vis_angle/ 2, [0, 360], [0, 2*np.pi])
+        end = agent.scale(direction_angle + vis_angle / 2, [0, 360], [0, 2*np.pi])
+        t = np.linspace(start, end, num=20, dtype=np.float32)
+        x = np.append(0, radius * np.cos(t))
+        y = np.append(0, radius * np.sin(t))
+        area = np.c_[x, y]
+        return area + self.position
+
+    def debug_print(self, screen:pg.Surface):
+        pg.draw.circle(screen, (255, 0, 0), np.array(self.position, dtype=np.int32), 3)
+        sprite_dim = pg.Vector2(self.showable_sprite.get_size())
+        pg.draw.rect(screen, (0, 255, 0), pg.Rect(self.position - sprite_dim/2, sprite_dim), 2)
+        pg.draw.polygon(screen, (0, 0, 0), self.reaction_area(), 2)
+
+
