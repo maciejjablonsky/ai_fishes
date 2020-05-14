413d7d74729620d799d4f6939ad66b1d4f897588
maciejjablonsky <maciekjablonsky@gmail.com>
2020-05-15 01:56:32 +0200

    Added finding neighbours of given agent

--- aifishes/environment.py
+++ aifishes/environment.py
@@ -4,6 +4,7 @@ from aifishes.fish import Fish
 from aifishes.predator import Predator
 import scipy as scp
 from smartquadtree import Quadtree
+from aifishes.agent import Agent
 
 QTREE_THRESHOLD = 4
 
@@ -13,6 +14,7 @@ class Environment:
         self.fishes = [Fish() for _ in range(cfg.fish_amount())]
         self.predators = [Predator() for _ in range(cfg.predator_amount())]
         self.qtree = None
+        self.update_qtree()
 
     def get_state(self):        
         return {
@@ -27,6 +29,8 @@ class Environment:
             fish.apply_force(acc)
         for predator, acc in zip(self.predators, data['predator_acc']):
             predator.apply_force(acc)
+            predator.hunt(self.find_neighbours(predator))
+            self.find_neighbours(predator)
         for agent in self.fishes + self.predators:
             agent.update(dtime)
         self.update_qtree()
@@ -37,3 +41,10 @@ class Environment:
         w, h = cfg.borders()
         self.qtree = Quadtree(w/2, h/2, w/2, h/2, QTREE_THRESHOLD)
         [self.qtree.insert((agent.get_x(), agent.get_y(), agent)) for agent in self.fishes]
+
+    def find_neighbours(self, agent: Agent):
+        reaction_area = agent.reaction_area()
+        self.qtree.set_mask(reaction_area)
+        neighbours = self.qtree.elements()
+        result = [element[2] for element in neighbours]
+        return result
