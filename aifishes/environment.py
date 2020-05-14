4e8e572a4ab9a2de4e952b50b6601c6aa059e3b0
maciejjablonsky <maciekjablonsky@gmail.com>
2020-05-14 23:52:55 +0200

    Changed get_agents into get_state, added updating quadtree after every frame

--- aifishes/environment.py
+++ aifishes/environment.py
@@ -2,17 +2,23 @@ import aifishes.config as cfg
 import pygame
 from aifishes.fish import Fish
 from aifishes.predator import Predator
+import scipy as scp
+from smartquadtree import Quadtree
+
+QTREE_THRESHOLD = 4
 
 
 class Environment:
     def __init__(self):
         self.fishes = [Fish() for _ in range(cfg.fish_amount())]
         self.predators = [Predator() for _ in range(cfg.predator_amount())]
+        self.qtree = None
 
-    def get_agents(self):
+    def get_state(self):        
         return {
-            'fishes':self.fishes,
-            'predators':self.predators
+            'fishes': self.fishes,
+            'predators': self.predators,
+            'qtree': self.qtree
         }
 
     def frame(self, data: dict):
@@ -23,3 +29,11 @@ class Environment:
             predator.apply_force(acc)
         for agent in self.fishes + self.predators:
             agent.update(dtime)
+        self.update_qtree()
+
+
+    def update_qtree(self):
+        """ qtree takes center x, y and then width and heigth, so region is described as (x - w, y - h, x + w, y + h)"""
+        w, h = cfg.borders()
+        self.qtree = Quadtree(w/2, h/2, w/2, h/2, QTREE_THRESHOLD)
+        [self.qtree.insert((agent.get_x(), agent.get_y(), agent)) for agent in self.fishes]
