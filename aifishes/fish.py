62eab09880ee683afb6cc467b00c9818087ba518
maciejjablonsky <maciekjablonsky@gmail.com>
2020-05-14 23:54:02 +0200

    Added limiting velocity with different limits for fishes and predators

--- aifishes/fish.py
+++ aifishes/fish.py
@@ -4,21 +4,27 @@ import aifishes.config as cfg
 from aifishes.agent import Agent, random_position, random_velocity
 import pygame as pg
 
-FISH_COLOR=pg.Color('chartreuse')
+FISH_COLOR = pg.Color('chartreuse')
+FISH_SPRITE = None
 
-def create_fish_surface():
-        dim = cfg.fish_dim()
+
+def fish_sprite():
+    global FISH_SPRITE
+    if FISH_SPRITE is None:
+        dim = np.array(cfg.fish_dim())
         surf = pg.Surface(dim, pg.SRCALPHA)
         shape = np.array([[0, 0], [dim[0], 0.5 * dim[1]],[0, dim[1]]], dtype=np.float32)
+
         pg.gfxdraw.aapolygon(surf, shape, FISH_COLOR)
         pg.gfxdraw.filled_polygon(surf, shape, FISH_COLOR)
-        return surf
+        FISH_SPRITE = surf
+    return FISH_SPRITE
 
-
-fish_surface = create_fish_surface()
 
 class Fish(Agent):
     def __init__(self):
-        super().__init__(fish_surface,  random_position(), random_velocity())
+        super().__init__(fish_sprite(), random_position(), random_velocity(cfg.fish_vel_start_magnitude()))
 
-    
+    def limit_velocity(self):
+        limit = cfg.fish_vel_max_magnitude()
+        return super().limit_velocity(limit=limit)
