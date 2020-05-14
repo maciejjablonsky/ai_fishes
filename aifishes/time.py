321bfa25abaff938aad967e361610727b918fc63
maciejjablonsky <maciekjablonsky@gmail.com>
2020-05-14 21:54:15 +0200

    Added dtime and global time counters

--- aifishes/time.py
+++ aifishes/time.py
@@ -5,15 +5,22 @@ class Time():
         self.clock = pg.time.Clock()
         self.last_ticks = 0
         self.dtime = 0
+        self.gtime = 0
 
     def tick(self):
         t = pg.time.get_ticks()
         self.dtime = (t - self.last_ticks) / 1000
+        self.gtime += self.dtime
         self.last_ticks = t
         self.clock.tick()
     
     def get_dtime(self):
+        """Returns time since last frame in miliseconds"""
         return self.dtime
+
+    def get_gtime(self):
+        """Returns global time since setup() call"""
+        return self.gtime
     
     def get_fps(self):
         return self.clock.get_fps()
