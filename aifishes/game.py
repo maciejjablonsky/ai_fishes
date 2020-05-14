da61d799dbe88199d9073e946bf089e93ffc3fb8
maciejjablonsky <maciekjablonsky@gmail.com>
2020-05-14 21:52:24 +0200

    Added event handler to enable closing window and restarting environment

--- aifishes/game.py
+++ aifishes/game.py
@@ -4,20 +4,18 @@ from aifishes.environment import Environment
 from aifishes.time import Time
 import aifishes.config as cfg
 import numpy as np
-
+import math
 COLORS = pg.colordict.THECOLORS
-
 
 SCREEN_COLOR = COLORS['darkmagenta']
 SHOW_FPS = True
 
+
 class Game:
     def __init__(self):
         pg.init()
-        self.width, self.height = cfg.borders()
         self.env = None
-        self.screen = pg.display.set_mode(
-            pg.Rect(0, 0, self.width, self.height).size)
+        self.screen = pg.display.set_mode(size=cfg.borders())
         self.time = Time()
         pg.font.init()
         self.font = pg.font.Font(None, 30)
@@ -31,16 +29,17 @@ class Game:
         agents = self.env.get_agents()
         data = {
             'dtime': self.time.get_dtime(),
-            'fish_acc': [pg.Vector2(0,0)] * len(agents[0]),
-            'predator_acc': [pg.Vector2(0,0)] * len(agents[1])
+            'fish_acc': [pg.Vector2(0, 0)] * len(agents['fishes']),
+            'predator_acc': [pg.Vector2(0, 0)] * len(agents['predators'])
         }
         self.env.frame(data)
 
     def draw(self):
         self.screen.fill(SCREEN_COLOR)
-        for agent in self.env.get_agents()[0]:
+        for agent in self.env.get_agents()['fishes']:
             self.screen.blit(agent.get_showable(), agent.position)
-        if SHOW_FPS: self.blit_fps(self.time.get_fps())
+        if SHOW_FPS:
+            self.blit_fps(self.time.get_fps())
         pg.display.flip()
 
     def blit_fps(self, fps):
@@ -48,12 +47,17 @@ class Game:
             "FPS: {0:.2f}".format(fps), True, pg.Color('red'))
         self.screen.blit(fps_view, (10, 10))
 
-
     def run(self):
         while self.running:
-            for event in pg.event.get():
-                if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
-                    return
+            self.events()
             self.update()
             self.draw()
             self.time.tick()
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_r:
+                    self.setup()
