b58ec948e0c532ba4f84a66d3e9ec65d8b9210f3
Marek.Hering <marek.hering99@gmail.com>
2020-05-17 19:58:38 +0200

    Added grid

--- aifishes/qlearning.py
+++ aifishes/qlearning.py
@@ -10,8 +10,13 @@ class QLearing():
         self.alfa = cfg.qlearing()['alfa']
         self.number_of_directions = cfg.qlearing()['number_of_directions']
         self.qtable = np.zeros([self.resolution[0], self.resolution[1], self.number_of_directions])
+        # Debug
+        self.grid = cfg.qlearing()['print_grid']
 
     def next_step(self, last_states):
+        if self.grid:
+            self.print_grid()
+
         if last_states:
             acc_table = []
             for fish in last_states['all_fishes']:
@@ -58,3 +63,12 @@ class QLearing():
             acceleration += temp
 
         return acceleration
+
+    def print_grid(self):
+        screen = pg.display.get_surface()
+        space_x = self.dim[0] / self.resolution[0]
+        space_y = self.dim[1] / self.resolution[1]
+        for i in range (self.resolution[0]):
+            pg.draw.line(screen, pg.Color('Black'), [i * space_x, 0], [i * space_x, self.dim[1]], 2)
+        for i in range (self.resolution[1]):
+            pg.draw.line(screen, pg.Color('Black'), [0, i * space_y], [self.dim[0], i * space_y], 2)
