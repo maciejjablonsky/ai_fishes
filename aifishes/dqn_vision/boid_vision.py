474ae2cc07e821c263db2ccd668380e0606911db
maciejjablonsky <maciekjablonsky@gmail.com>
2020-06-01 01:42:09 +0200

    Removed unnecessary computing

--- aifishes/dqn_vision/boid_vision.py
+++ aifishes/dqn_vision/boid_vision.py
@@ -11,20 +11,16 @@ def boid_reaction_area_surface(boid, frame: int) -> torch.Tensor:
     screen = pg.display.get_surface()
     vision_area = boid.reaction_area()
     position = boid.position
-    x = vision_area[:, 0]
-    y = vision_area[:, 1]
     radius = cfg.fish()['reaction_radius']
 
     left = int(max(0, round(position.x - radius)))
     up = int(max(0, round(position.y - radius)))
     right = int(min(screen.get_width() - 1, round(position.x + radius)))
-    bottom = int(min(screen.get_height() -1 , round(position.y + radius)))
-    width = right - left
-    height = bottom - up
-    bounding_box = pg.Rect(left, up, width, height)
-    vision_surface = pg.Surface((cfg.fish()['reaction_radius'] * 2, cfg.fish()['reaction_radius'] * 2))
+    bottom = int(min(screen.get_height() - 1, round(position.y + radius)))
+    bounding_box = pg.Rect(left, up, right - left, bottom - up)
+    vision_surface = pg.Surface((radius * 2, radius * 2))
+    clipped = vision_surface.copy()
     dest = (max(0, radius - position.x), max(0, radius - position.y))
     vision_surface.blit(screen,dest=dest, area=bounding_box)
-    clipped = pg.Surface((cfg.fish()['reaction_radius'] * 2, cfg.fish()['reaction_radius'] * 2))
-    textured_polygon(clipped, vision_area - position + pg.Vector2(radius, radius), vision_surface, 0, 0)
-    # pg.image.save(clipped, 'tex_boid_%d.png' % frame)
+    textured_polygon(clipped, vision_area - position + pg.Vector2(radius, radius), vision_surface, 20, 0)
+    pg.image.save(clipped, 'tex_boid_%d.png' % frame)
