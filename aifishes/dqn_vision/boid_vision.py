93392ec6eac66cdfe80f140adc2f123e4e938992
maciejjablonsky <maciekjablonsky@gmail.com>
2020-06-01 03:18:10 +0200

    Added saving reaction area as reference array to pixels

--- aifishes/dqn_vision/boid_vision.py
+++ aifishes/dqn_vision/boid_vision.py
@@ -4,12 +4,25 @@ import pygame as pg
 from pygame.gfxdraw import textured_polygon
 import torch
 import aifishes.config as cfg
+from shapely.geometry import Point, LineString, Polygon
+
+screen_width, screen_height = cfg.environment()['dim']
 
 
 def boid_reaction_area_surface(boid, frame: int) -> torch.Tensor:
     '''Cutting boid reaction area from game screen'''
     screen = pg.display.get_surface()
     vision_area = boid.reaction_area()
+
+    for point in vision_area:
+        if point[0] < 0:
+            point[0] = 0
+        elif point[0] >= screen_width:
+            point[0] = screen_width
+        if point[1] < 0:
+            point[1] = 0
+        elif point[1] >= screen_height:
+            point[1] = screen_height
     position = boid.position
     radius = cfg.fish()['reaction_radius']
 
@@ -22,5 +35,5 @@ def boid_reaction_area_surface(boid, frame: int) -> torch.Tensor:
     clipped = vision_surface.copy()
     dest = (max(0, radius - position.x), max(0, radius - position.y))
     vision_surface.blit(screen,dest=dest, area=bounding_box)
-    textured_polygon(clipped, vision_area - position + pg.Vector2(radius, radius), vision_surface, 20, 0)
-    pg.image.save(clipped, 'tex_boid_%d.png' % frame)
+    textured_polygon(clipped, vision_area - position + pg.Vector2(radius, radius), vision_surface, 0, 0)
+    return pg.surfarray.pixels3d(clipped)
