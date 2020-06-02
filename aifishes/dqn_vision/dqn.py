e278bf0510caebab865917cf02326e7651bb11c9
maciejjablonsky <maciekjablonsky@gmail.com>
2020-06-02 12:57:41 +0200

    Changed number of channel inputs

--- aifishes/dqn_vision/dqn.py
+++ aifishes/dqn_vision/dqn.py
@@ -1,10 +1,11 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 class DQN(nn.Module):
 
     def __init__(self, h, w, outputs):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
         self.bn2 = nn.BatchNorm2d(32)
