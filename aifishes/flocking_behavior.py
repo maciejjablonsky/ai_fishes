a1940c9dd415e9d88246206ed122229a12267025
maciejjablonsky <maciekjablonsky@gmail.com>
2020-06-06 13:39:05 +0200

    Improved flocking behavior

--- aifishes/flocking_behavior.py
+++ aifishes/flocking_behavior.py
@@ -33,27 +33,28 @@ def flocking_behavior(environment_data: dict) -> List[Fish]:
     def separation(boid: Fish, flockmates: List[Fish]) -> Vector2:
         '''Computes acceleration to avoid local crowding with flockmates'''
         '''Formula is negative sum of differences between flockmates and boid'''
-        return  -1 *reduce(lambda a, b: a + b, [mate.position - boid.position for mate in flockmates])  * dtime
+        if len(flockmates) >0:
+            return -1 * reduce(lambda a, b: a + b, [mate.position - boid.position for mate in flockmates])
+        else:
+            return Vector2(0,0)
 
     def cohesion(boid: Fish, flockmates: List[Fish]) -> Vector2:
         '''Computes acceleration to keep boid close to other boids'''
         '''Formula is difference between average position of flockmates and boid position'''
         mates_positions = [mate.position for mate in flockmates]
-        return (reduce(lambda a, b: a + b, mates_positions)/len(flockmates) - boid.position) * (dtime)
+        return (reduce(lambda a, b: a + b, mates_positions)/len(flockmates) - boid.position)
 
     def alignment(boid: Fish, flockmates: List[Fish]) -> Vector2:
         '''Computes acceleration to keep boid velocity in alignment with local flockmates'''
         mates_velocities = [mate.velocity for mate in flockmates]
 
-        return (reduce(lambda a, b: a+b, mates_velocities) / len(flockmates) - boid.velocity) / dtime
+        return (reduce(lambda a, b: a+b, mates_velocities) / len(flockmates) - boid.velocity)
 
     def compute_flocking(boid: Fish) -> Vector2:
         '''Accumulates all traits for one boid'''
         flockmates = find_flockmates(boid)
         if len(flockmates) > 0:
-            return cohesion_coefficient * cohesion(boid, flockmates) + alignment_coefficient * alignment(boid, flockmates)
-            +  separation_coefficient * \
-                separation(boid, find_safe_space_intruders(boid))
+            return cohesion_coefficient * cohesion(boid, flockmates) + alignment_coefficient * alignment(boid, flockmates) + separation_coefficient * separation(boid, find_safe_space_intruders(boid))
         else:
             return Vector2(0, 0)
 
