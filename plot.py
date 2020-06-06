72ac9f8dcede5d0eb861fbc059a04c518b146914
maciejjablonsky <maciekjablonsky@gmail.com>
2020-06-06 22:40:19 +0200

    Added script to create plots of boids lifetime

--- plot.py
+++ plot.py
@@ -1,24 +1,43 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import sys 
-data = pd.read_csv(sys.argv[1])
-x = data['epochs']
-avg = data['average']
-max_ = data['maximum']
-max_z = np.polyfit(x, max_, 1)
-max_p = np.poly1d(max_z)
-max_mean = sum(max_) / len(max_)
-plt.plot(x, [max_mean]*len(x), "r--", color='green')
-plt.plot(x, max_p(x), "r--")
-z = np.polyfit(x, avg, 1)
-p = np.poly1d(z)
+import sys
+from collections import namedtuple
 
-avg_mean = sum(avg)/len(avg)
-plt.plot(x, [avg_mean] * len(x), "r--", color='green')
-plt.plot(x,p(x),"r--")
-plt.plot(data['average'], label='avg')
-plt.plot(data['maximum'], label='max')
+flocking= pd.read_csv(sys.argv[1])
+qlearning = pd.read_csv(sys.argv[2])
+
+# max_p = np.poly1d(np.polyfit(flocking['epochs'],flocking['maxiumum'], 1))
+# max_mean = sum(flocking['maximum']) / len(flocking['maximum'])
+
+plot_of_max = plt.figure(figsize=(8,6))
+# plot_of_max.plot(x, [max_mean]*len(x), "--",
+        #  label='Mean of maximum lifetime with flocking')
+# plt.plot(x, max_p(x), "r--")
+# z = np.polyfit(x, avg, 1)
+# p = np.poly1d(z)
+max_histogram = flocking['maximum'] - qlearning['maximum']
+plt.title('Wykres maksymalnego czasu życia')
+plt.plot(flocking['epochs'], flocking['maximum'], label='Maksymalny czas życia z zachowaniem ławicy')
+plt.plot(qlearning['epochs'],qlearning['maximum'], label='Maksymalny czas życia bez zachowania ławicy')
+plt.bar(flocking['epochs'], max_histogram, width=0.9, label='Różnica pomiędzy wartościami krzywych')
+plt.xlabel('Liczba epok [n]')
+plt.ylabel('Liczba przeżytych klatek')
 plt.legend()
+plt.grid()
+plt.tight_layout()
+plt.show()
+
+
+average_historgram = flocking['average'] - qlearning['average']
+plt.figure(figsize=(8,6))
+plt.title('Wykres średniego czasu życia')
+plt.plot(flocking['epochs'], flocking['average'], label='Średnia z zachowaniem ławicy')
+plt.plot(qlearning['epochs'], qlearning['average'], label='Średnia bez zachowania ławicy')
+plt.bar(flocking['epochs'], average_historgram, width=0.9, label='Różnica pomiędzy wartościami krzywych')
+plt.xlabel('Liczba epok [n]')
+plt.ylabel('Liczba przeżytych klatek')
+plt.legend()
+plt.tight_layout()
 plt.grid()
 plt.show()
