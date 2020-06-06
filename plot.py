9232b7f766a248ab2524bb948929001f2224a237
maciejjablonsky <maciekjablonsky@gmail.com>
2020-06-06 16:21:25 +0200

    Increased epoch duration

--- plot.py
+++ plot.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-data = pd.read_csv('time_stats.csv')
-x = data['epoch']
-avg = data['avg']
-max_ = data['max']
+import sys 
+data = pd.read_csv(sys.argv[1])
+x = data['epochs']
+avg = data['average']
+max_ = data['maximum']
 max_z = np.polyfit(x, max_, 1)
 max_p = np.poly1d(max_z)
 plt.plot(x, max_p(x), "r--")
@@ -13,8 +13,8 @@ z = np.polyfit(x, avg, 1)
 p = np.poly1d(z)
 
 plt.plot(x,p(x),"r--")
-plt.plot(data['avg'], label='avg')
-plt.plot(data['max'], label='max')
+plt.plot(data['average'], label='avg')
+plt.plot(data['maximum'], label='max')
 plt.legend()
 plt.grid()
 plt.show()
